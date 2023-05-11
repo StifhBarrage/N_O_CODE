3622d627b9e372be50c2954bcb1a6ca5cca39c90
Stifh BL <stifhlife@gmail.com>
2023-05-11 16:39:35 -0500

    Start over

--- Hello_world.py
+++
@@ -1,5 +0,0 @@
-
-print("Hello World main")
-print('welcome to the jungle!')
-print('hello world')
-
---
+++ INTRODUCTION.py
@@ -0,0 +1,2 @@
+print('This is the beginning of the most interesting history about two entrepreneurs')
+
