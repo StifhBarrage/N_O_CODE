13973b18f80c1a2508e0e0289084110f1252f262
Stifh BL <stifhlife@gmail.com>
2023-05-12 22:26:54 -0500

    script python

---
+++ Stifh_folder/scripts/scripts.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+
+# Ruta del programa .exe que quieres abrir
+ruta_programa = 'C:\\Users\\ACERa\\Downloads\\MegaDownloader\\pVPN.exe'
+
+# Tiempo en segundos que quieres que el programa esté abierto antes de cerrarlo
+tiempo_abierto = 60
+
+# Tiempo en segundos que quieres esperar antes de abrir el programa de nuevo
+tiempo_espera = 1
+
+# Bucle infinito
+while True:
+    # Abre el programa
+    subprocess.Popen(ruta_programa)
+
+    # Espera el tiempo indicado antes de cerrar el programa
+    time.sleep(tiempo_abierto)
+
+    # Cierra el programa
+    subprocess.call(['taskkill', '/F', '/T', '/IM', 'pVPN.exe'])
+
+    # Espera el tiempo indicado antes de abrir el programa de nuevo
+    time.sleep(tiempo_espera)
