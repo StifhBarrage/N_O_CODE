55b2c3e1b5f08a8e6fd6f1e89f0dcf0c52c27f17
stifhbl <hbarraganl@unal.edu.co>
2023-05-25 19:46:11 -0500

    js prompt

---
+++ Stifh_folder/JavaScript_folder/01-Introduccion/js/app.js
@@ -0,0 +1,14 @@
+// name declaration
+const first_name = prompt("¿Cual es tu nombre?");
+// print in index.html file - you can see the result in the browser
+if (first_name !== '') {
+  document.querySelector(
+    ".contenido"
+  ).innerHTML = `${first_name} welcome to this new life style`;
+} else {}
+
+
+
+
+
+    
