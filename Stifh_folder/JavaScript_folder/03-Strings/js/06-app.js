034f0f642f02089beafa4b12abfc6e959a1f410c
stifhbl <hbarraganl@unal.edu.co>
2023-05-31 19:26:36 -0500

    string secction completed

--- Stifh_folder/JavaScript_folder/03-Strings/js/06-app.js
+++ Stifh_folder/JavaScript_folder/03-Strings/js/06-app.js
@@ -1,1 +1,16 @@
 const producto06_1 = 'Monitor 20 pulgadas';
+
+const texto = ' en Promocion'.repeat(3);
+console.log(texto);
+console.log(`${producto06_1} ${texto} !!!`);
+
+// SPLIT
+const actividad = 'working on my project';
+console.log(actividad.split(' '));
+
+const hobbies = 'read, walk, listen music, write, \n\
+                learn to program';
+console.log(hobbies.split(', '));
+
+const tweet = 'Learn to program from zero #JS';
+console.log(tweet.split('#'));
---
+++ Stifh_folder/JavaScript_folder/03-Strings/js/07-app.js
@@ -0,0 +1,13 @@
+const producto07_1 = 'Monitor 20 pulgadas';
+
+console.log(producto07_1.toUpperCase());
+console.log(producto07_1.toLowerCase());
+
+const email = "hBArraganl@unal.edu.co"
+console.log(email.toLowerCase());
+const email2 = "STIFHBL@gimail.com"
+console.log(email2.toLowerCase());
+
+const precio = 300;
+console.log(precio.toString().split(''));
+
