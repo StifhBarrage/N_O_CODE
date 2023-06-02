5eb66e0f60afdd65479c69a28b62e7e5e6d4f952
JPArt22 <jhprieto@unal.edu.co>
2023-06-01 22:48:23 -0500

    Millonarios FC

---
+++ JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,18 @@
+const name = (prompt('MILONARIOS FC Champion 2023 \n ¿Cuál es tu nombre?'))
+if (name == 'Jhon'|| name == "jhon") {
+    document.querySelector('.contenido').innerHTML = `${name}, eres arte bro`;
+} else if (name == 'Stifh' || name == 'stifh') {
+    document.querySelector('.contenido').innerHTML = `${name}, sos cacorro
+    \nConviértete al azul!`;
+} else {
+    saludar();
+}
+
+function saludar() {
+    const cond = (prompt('¿Eres de Millonarios FC?'));
+if (cond == 'Yes' || cond == 'yes' || cond == 'si' || cond == 'Si') { 
+    document.querySelector('.contenido').innerHTML = `Eres arte bro, soloMillos`;
+}   else {  
+    document.querySelector('.contenido').innerHTML = 'Sos cacorro\nConviértete al azul!';
+}
+}
