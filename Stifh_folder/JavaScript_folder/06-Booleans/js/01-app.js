ee194f21b75c585c10cdaf8a1ebf6fb8d0ae5929
stifhbl <hbarraganl@unal.edu.co>
2023-08-10 11:36:18 -0500

    booleans

---
+++ Stifh_folder/JavaScript_folder/06-Booleans/js/01-app.js
@@ -0,0 +1,9 @@
+const boolean1_1 = true;d
+const boolean1_2 = false;
+const boolean1_3 = "true";
+console.log(boolean1_1);
+console.log(boolean1_2);
+
+console.log(typeof boolean1_1);
+
+
---
+++ Stifh_folder/JavaScript_folder/06-Booleans/js/02-app.js
@@ -0,0 +1,4 @@
+const boolean2_1 = true;
+const boolean2_2 = false;
+console.log(boolean2_1 === boolean2_2);
+console.log(boolean2_1 === true);
