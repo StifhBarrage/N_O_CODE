e56e59e76dcf212fc3efe250986b918553cd9e7a
JPArt22 <jhprieto@unal.edu.co>
2023-08-16 18:30:34 -0500

    Numbers

---
+++ JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/04-Numeros/js/04-app.js
@@ -0,0 +1,10 @@
+let resultado;
+
+resultado = 20 + 30 * 2;
+resultado = (20 + 30) * 2; //Primero se ejecuta lo que esta dentro de los parentesis
+resultado = (100 + 200 + 300) * .2; //20% de descuento
+resultado = (100 + 200 + 300) * 1.16; //16% de impuesto
+
+
+
+console.log(resultado);
