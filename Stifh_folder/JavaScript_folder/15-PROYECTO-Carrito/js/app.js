4b2c157bb8d2f38380d95a0d134d733e110411ad
stifhbl <hbarraganl@unal.edu.co>
2023-08-23 18:17:33 -0500

    update amount and remove

--- Stifh_folder/JavaScript_folder/15-PROYECTO-Carrito/js/app.js
+++ Stifh_folder/JavaScript_folder/15-PROYECTO-Carrito/js/app.js
@@ -35,8 +35,63 @@ const readDataCourse = (course) => {
         id: course.querySelector('a').getAttribute('data-id'),
         amount: 1
     }
+    // check if the course is already in the shopping cart
+    const exist = itemsCart.some( course => course.id === infoCourse.id);
+    if(exist){
+        // update the amount
+        const courses = itemsCart.map( course => {
+            if(course.id === infoCourse.id){
+                course.amount++;
+                return course; // return the object updated
+            }else{
+                return course; // return the objects that are not duplicated
+            }
+        });
+        itemsCart = [...courses];
+    }
+
     // add elements to the shopping cart
     itemsCart = [...itemsCart, infoCourse];
     console.log(itemsCart);
+    showShoppingCart();
+}
+
+// function for show the shopping cart in the HTML
+const showShoppingCart = () => {
+
+    // clear the HTML
+    clearHTML();
+
+    // build the HTML
+    itemsCart.forEach( course => {
+        // tr is a row in the table
+        const {img, title, price, amount, id} = course;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            
+            <img src="${course.imag}" width="100">
+        </td>
+        <td>${title}</td>
+        <td>${price}</td>
+        <td>${amount}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${id}">X</a>
+        </td>
+        `;
+
+        // add the HTML of the shopping cart
+        cartContainer.appendChild(row);
+    });
+}
+
+// function for clear the courses of the tbody
+const clearHTML = () => {
+    // slow way
+    // cartContainer.innerHTML = '';
+    while(cartContainer.firstChild){
+        cartContainer.removeChild(cartContainer.firstChild);
+    }
+
 }
 
