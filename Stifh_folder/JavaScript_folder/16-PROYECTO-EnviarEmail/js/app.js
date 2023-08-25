8013063e4d71e018a193bc4227e18efc7307a742
stifhbl <hbarraganl@unal.edu.co>
2023-08-25 17:32:34 -0500

    send - reset - spinner

--- Stifh_folder/JavaScript_folder/16-PROYECTO-EnviarEmail/js/app.js
+++ Stifh_folder/JavaScript_folder/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,27 +1,73 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    const email = {
+        email: '',
+        subject: '',
+        message: ''
+    }
     
     // interface elements
     const inputEmail = document.querySelector('#email');
-    const inputSubject = document.querySelector('#asunto');
-    const inputMessage = document.querySelector('#mensaje');
+    const inputSubject = document.querySelector('#subject');
+    const inputMessage = document.querySelector('#message');
     const form = document.querySelector('#formulario'); 
+    const btnSummit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     // assign events
-    inputEmail.addEventListener('blur', validate);
-    inputSubject.addEventListener('blur', validate);
-    inputMessage.addEventListener('blur',validate);
+    inputEmail.addEventListener('input', validate);
+    inputSubject.addEventListener('input', validate);
+    inputMessage.addEventListener('input',validate);
+
+    form.addEventListener('submit', sendEmail);
+
+    btnReset.addEventListener('click', function(e) {
+        e.preventDefault();
+        resetForm();
+
+    });
+
+    function sendEmail(e) {
+        e.preventDefault();
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+
+            resetForm();
+
+        }, 3000);
+    }
 
     function validate(e) {
 
-
         if (e.target.value.trim() === '') {
             showAlert(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-            console.log('No hay nada');
+            email[e.target.name] = '';
+            checkEmailObject();
+            return;
+        }
+         if ( e.target.id === 'email' && !validateEmail(e.target.value)) {
+            showAlert(`El campo ${e.target.id} no es válido`, e.target.parentElement);
+            email[e.target.name] = '';
+            checkEmailObject();
             return;
         }
         clearAlert(e.target.parentElement);
+
+        // add the value to the object
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+        // check the email object
+        checkEmailObject();
+
+        
     }
 
     function showAlert(message, reference) {
@@ -38,19 +84,45 @@ document.addEventListener('DOMContentLoaded', function() {
         const error = document.createElement('P');
         error.textContent = message;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
-        console.log(error);
-
         // insert in the form
         reference.appendChild(error);
     }
-    function clearAlert() {
+
+    function clearAlert(reference) {
         const alert = reference.querySelector('.bg-red-600');
         if (alert) {
             alert.remove();
         }
     }
 
-    
+    function validateEmail(email) {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;;
+        const resultado = regex.test(email);
+        return resultado;
+    }
 
+    function checkEmailObject() {
+        console.log(email);
+        if (Object.values(email).includes('')) {
+            btnSummit.classList.add( 'opacity-50');
+            btnSummit.disabled = true;
+            return 
+
+        }
+        btnSummit.classList.remove( 'opacity-50');
+        btnSummit.disabled = false;
+    }
+
+    function resetForm() {
+            // reset the form
+            email.email = '';
+            email.subject = '';
+            email.message = '';
+    
+            form.reset();
+            checkEmailObject();
+    }
 
 });
+
+
