8d6cd82a1bdb037d3a68eccfbb291700e41752ef
stifhbl <hbarraganl@unal.edu.co>
2023-08-24 21:05:00 -0500

    second project part_1

---
+++ Stifh_folder/JavaScript_folder/16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // interface elements
+    const email = document.querySelector('#email');
+    const asunto = document.querySelector('#asunto');
+    const mensaje = document.querySelector('#mensaje');
+    const btnEnviar = document.querySelector('#enviar');
+    const btnReset = document.querySelector('#resetBtn');
+    const formularioEnviar = document.querySelector('#enviar-mail');
+    
+    
+});
