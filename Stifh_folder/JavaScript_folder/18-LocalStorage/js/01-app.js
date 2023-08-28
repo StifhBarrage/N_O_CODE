b97f7181c671bc7c6ddb37443113f45b33f41842
stifhbl <hbarraganl@unal.edu.co>
2023-08-28 18:26:31 -0500

    localStorage get Obj/Array

--- Stifh_folder/JavaScript_folder/18-LocalStorage/js/01-app.js
+++ Stifh_folder/JavaScript_folder/18-LocalStorage/js/01-app.js
@@ -1,5 +1,9 @@
+////////// SET ITEM TO LOCAL STORAGE //////////
+
+// Local storage for all the time
 localStorage.setItem('nombre', 'Juan')
 
+// Session storage for one session 
 sessionStorage.setItem('nombre', 'Juan')
 
 const producto = {
@@ -7,9 +11,24 @@ const producto = {
     precio: 300
 }
 
+// add to local storage a object
 const productoString = JSON.stringify(producto)
-console.log(productoString)
 localStorage.setItem('producto', productoString)
 
+// add to local storage a array
 const meses = ['Enero', 'Febrero', 'Marzo']
 localStorage.setItem('meses', JSON.stringify(meses))
+
+////////// GET ITEM FROM LOCAL STORAGE ///////////
+
+// get item from local storage
+const nombre = localStorage.getItem('nombre')
+console.log(nombre)
+
+// get a object from local storage
+const productoJSON = localStorage.getItem('producto')
+console.log(JSON.parse(productoJSON))
+
+// get a array from local storage
+const mesesArray = JSON.parse(localStorage.getItem('meses'))
+console.log(mesesArray)
