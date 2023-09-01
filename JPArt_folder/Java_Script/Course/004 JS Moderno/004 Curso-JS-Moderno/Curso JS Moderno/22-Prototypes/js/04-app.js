e4b8d5df649c7a70f91d07097f6ca1b584ad75b6
JPArt22 <jhprieto@unal.edu.co>
2023-09-01 14:45:32 -0500

    Prototypes Done

---
+++ JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/22-Prototypes/js/04-app.js
@@ -0,0 +1,41 @@
+function Cliente (nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo
+
+}
+
+Cliente.prototype.tipoCliente = function() {
+    let tipo;
+    if (this.saldo > 10000) {
+        tipo = 'Gold'
+    } else if (this.saldo > 5000) {
+        tipo = 'Platinium'
+    } else {
+        tipo = 'Normal'
+    }
+    return tipo; 
+    }
+
+Cliente.prototype.nombreClienteSaldo = function () {
+    return `Nombre: ${this.nombre}, Saldo: ${this.saldo}, Tipo de cliente: ${this.tipoCliente()} `
+}
+
+Cliente.prototype.retiraSaldo = function(retira) {
+    this.saldo -= retira
+}
+
+
+
+function Persona (nombre, saldo, telefono) {
+    Cliente.call(this, nombre, saldo); // Heredar nombre y saldo de cliente a persona
+    this.telefono = telefono
+}
+
+Persona.prototype = Object.create (Cliente.prototype); // Copiar el prototype de cliente
+
+Persona.prototype.constructor = Cliente;
+
+
+const juan = new Persona ('Juan', 5000, 13258965);
+console.log(juan);
+console.log(juan.nombreClienteSaldo());
