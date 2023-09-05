a899476db42cd8357901b302bb1874da8a18edd2
JPArt22 <jhprieto@unal.edu.co>
2023-09-05 05:06:51 -0500

    POO project 04

---
+++ JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -0,0 +1,45 @@
+// Variables
+const formulario = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
+
+
+// Event Listeners
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+}
+
+
+// Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+
+}
+
+class UI {
+
+}
+
+// Instanciar
+const ui = new UI();
+let presupuesto;
+
+// Funciones
+function preguntarPresupuesto() {
+    const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
+
+    // console.log(Number(presupuestoUsuario));
+
+    if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+        window.location.reload();
+    }
+
+    // Presupuesto válido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
+}
