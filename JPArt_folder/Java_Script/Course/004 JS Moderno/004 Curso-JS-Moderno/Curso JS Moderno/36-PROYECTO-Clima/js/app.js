f781b1a0e42ccadef80b3a23484e552eae9681d3
JPArt22 <jhprieto@unal.edu.co>
2023-10-18 13:47:00 -0500

    API_Clima project DONE

--- JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/36-PROYECTO-Clima/js/app.js
+++ JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/36-PROYECTO-Clima/js/app.js
@@ -54,14 +54,87 @@ function consultarAPI(ciudad, pais) {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
 
+    Spinner(); // Muestra un spinner de carga
+
     fetch(url)
         .then( respuesta => respuesta.json() )
         .then(datos => {
-            console.log(datos)
+
+            limpiarHTML(); // Limpiar el HTML previo
+
             if (datos.cod === "404") {
                 mostrarError('Ciudad no encontrada');
+                return;
             }
+
+            // Imprime la respuesta en el HTML
+            mostrarClima(datos);
+
         })
+
+
+}
+
+function mostrarClima(datos) {
+    const { name, main: { temp, temp_max, temp_min } } = datos; // Destructuring
+   
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados.toFixed(2)} &#8451;`; // toFixed(2) para que solo muestre 2 decimales
+    actual.classList.add('font-bold', 'text-6xl', 'text-white');
+
+    const tempMaxima = document.createElement('p');
+    tempMaxima.innerHTML = `Max: ${max} &#8451;`;   
+    tempMaxima.classList.add('text-xl', 'text-red-900');
+
+    const tempMinima = document.createElement('p');
+    tempMinima.innerHTML = `Min: ${min} &#8451;`;
+    tempMinima.classList.add('text-xl', 'text-blue-900');
+
+    const nombreCiudad = document.createElement('p');
+    nombreCiudad.innerHTML = `Clima en ${name}`;
+    nombreCiudad.classList.add('font-bold', 'text-2xl', 'text-black');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center');
+
+    var saltoDelinea = document.createElement("br");
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(saltoDelinea);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMinima);
+
+    resultado.appendChild(resultadoDiv); // resultado variable del inicio
+
+}
+
+function kelvinACentigrados(grados) {
+    return parseInt(grados - 273.15);
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function Spinner() {
+
+    limpiarHTML();
+    
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('double-bounce1');
+
+    divSpinner.innerHTML = `
+        <div class="double-bounce1"></div>
+        <div class="double-bounce2"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
 
 
 }
