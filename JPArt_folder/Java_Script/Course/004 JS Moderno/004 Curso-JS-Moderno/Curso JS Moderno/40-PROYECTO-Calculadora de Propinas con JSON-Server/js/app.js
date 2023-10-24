5c4ffd8354d3cd2c4ea4e821d33c336fd2080968
JPArt22 <jhprieto@unal.edu.co>
2023-10-24 12:59:31 -0500

    API REST Restaurant project 09

--- JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -18,13 +18,62 @@ function guardarCliente() {
     const camposVacios = [mesa, hora].some( campo => campo === '' );
 
     if( camposVacios ) {
-    
-        console.log('Todos los campos son obligatorios');
 
-    } else {
-        console.log('Todos los campos estan llenos');
-    }
+        //Verificar si hay una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if (!existeAlerta) {
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+            
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+       
+        }
+
+        return;
+
+    } 
     
+        //Asignar valores al objeto cliente
+        cliente = {...cliente, mesa, hora};
+
+        // console.log(cliente);
+
+        // Ocultar modal
+        const modalFormulario = document.querySelector('#formulario');   
+        const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+        modalBootstrap.hide();
+
+
+        // Mostrar secciones
+        mostrarSecciones();
+
+        // Obtener platillos de la api
+        obtenerPlatillos();
+
+    }
+
+
+
+
+function mostrarSecciones() {
+
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach( seccion => seccion.classList.remove('d-none') );
+
+}
+
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos';
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( resultado => console.log(resultado) )
+        .catch( error => console.log(error) );
 
 
 }
