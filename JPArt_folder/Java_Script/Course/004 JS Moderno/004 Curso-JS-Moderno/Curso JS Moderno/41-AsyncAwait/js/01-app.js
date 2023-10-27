4cbed52e753a9da455ed93d3b719d9f4a23fcc6b
JPArt22 <jhprieto@unal.edu.co>
2023-10-27 08:04:30 -0500

    Async/Await 02

---
+++ JPArt_folder/Java_Script/Course/004 JS Moderno/004 Curso-JS-Moderno/Curso JS Moderno/41-AsyncAwait/js/01-app.js
@@ -0,0 +1,19 @@
+// Qué es async await
+
+// async await es la forma más fácil de trabajar con promesas
+
+console.log(1+1);
+
+
+try {
+    hola();
+} catch (error) {
+    console.log(error);
+}
+
+
+console.log(2+2);
+
+// Try Catch 
+// es una forma de manejar los errores en js
+
