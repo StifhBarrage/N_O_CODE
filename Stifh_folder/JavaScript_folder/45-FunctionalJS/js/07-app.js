e40bb65bbb282309583b5e0304215b38276d4d4e
stifhbl <hbarraganl@unal.edu.co>
2023-11-13 23:56:32 -0500

    Function return a function rules

--- Stifh_folder/JavaScript_folder/45-FunctionalJS/js/07-app.js
+++ Stifh_folder/JavaScript_folder/45-FunctionalJS/js/07-app.js
@@ -1,2 +1,11 @@
-// funciones puras
+// Funciones que retornan funciones
+
+// En este caso la función obtenerCliente() retorna otra función que es la que se va a ejecutar
+const obtenerCliente = () => () => console.log('Juan');
+
+// fn es la función que retorna obtenerCliente()
+const fn = obtenerCliente();
+
+// Aqui se ejecuta la función que retorna obtenerCliente()
+fn();
 
